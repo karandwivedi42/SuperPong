2ee2272afab37bbd8077414c8820d39cfeb3f249
Karan Dwivedi <karan.dwivedi42@gmail.com>
2016-04-30 15:12:56 +0530

    Done?

--- Final/Receiver.java
+++ Final/Receiver.java
@@ -5,14 +5,12 @@ import java.lang.*;
 
 public class Receiver implements Runnable{
 
-	int Port;
+	int Port = 20000;
 	boolean isRunning;
 	DatagramSocket serverSocket;
 	Handler h;
-	Pinger p;
-		public Receiver(Handler h,int port)
+		public Receiver(Handler h)
 		{
-			this.Port = port;
 			this.h = h;
 			serverSocket = null;
 			try{
@@ -25,20 +23,6 @@ public class Receiver implements Runnable{
 			}
 		}
 		
-		public Receiver(Pinger p,int port)
-		{
-			this.Port = port;
-			this.p = p;
-			serverSocket = null;
-			try{
-				serverSocket = new DatagramSocket(Port);
-				//serverSocket.setSoTimeout(2000);
-			}
-			catch(SocketException e){
-				System.out.println(e.toString());
-				return;
-			}
-		}
 	@Override
 	public void run() {
 		//System.out.println("Receiver thread is running...");
@@ -54,75 +38,156 @@ public class Receiver implements Runnable{
 				serverSocket.receive(receivePacket);
 				String str = "DUMMYSTR";
 				str = new String(receivePacket.getData());
-				if(str.equals("ping"))
-				{
-					String con = "confirm";
-					byte[] sendD = con.getBytes();
-					DatagramPacket senderPacket = new DatagramPacket(sendD, sendD.length,receivePacket.getAddress(),receivePacket.getPort());
-					try{
-						serverSocket.send(senderPacket);
-					}
-					catch(IOException e){
-						System.out.println(e.toString()+"\nUnnable to send");
-					}
-				}
-				
-				
-				else{
-				
-				
 				System.out.println("str "+str);
 				String IP = "DUMMYIP";
 				IP = receivePacket.getAddress().toString().replace("/1","1");
 				System.out.println("IPofSender "+IP );
-				//////////////////////////////////////////////////////////////////////////////////
-				if(h.isServer)
+				
+				String[] splitt = str.split("~");
+				if(splitt[0].equals("Update"))
+				{
+					if(splitt[1].equals("movepaddle"))
 					{
-						String[] splitter = str.split("~");
-						if(splitter[0].equals("Hello"))
-						{	
-							String ips ="";
-							
-							for(Sender send: h.listOfSenders)
-							{
-								send.normalSend("FWD~"+IP+"~"+splitter[1]);
-								
-							}
-							
-							
-							for(Sender ss:h.listOfSenders)
-							{
-								ips = ips+ss.IP+"^";
-							}
-						
-						
-						Sender s = new Sender(this.Port,IP);
-						h.listOfSenders.add(s);
-						
-						//list ofIps also has to be added
-						
-						s.normalSend("Ack~"+"IPS~"+ips);
-						}
+					    String playerNo = splitt[2];
+					    String move = splitt[3];
+					    for(Player p : gamestate.players){
+					        if(p.name == splitt[2]){
+					            p.paddle.xc = Double.parseDouble(splitt[3]);
+					            p.paddle.yc = Double.parseDouble(splitt[4]);
+					        }
+					    }
+					    
 					}
-					else if(!h.isServer)
+					else if(splitt[1].equals("puckmove"))
 					{
-						String[] splitted = str.split("~");
-						if(splitted[0].equals("FWD"))
+					    String puckname = splitt[2];
+					    String x = splitt[3];
+					    String y = splitt[4];
+					    String velx = splitt[5];
+					    String vely = splitt[6];
+					    
+					    for(Puck p : gamestate.board.pucks){
+					        if(p.name == puckname){
+					            p.x = Double.parseDouble(x);
+					            p.y = Double.parseDouble(y);
+					            p.vx = Double.parseDouble(vx);
+					            p.vy = Double.parseDouble(vy);
+					        }
+					    }
+					    
+                    }
+                    else if(splitt[1].equals("gameStatus"))
+                    {
+                        gamestate.gameStatus = Integer.parseInt(splitt[2]);
+                    }
+                    
+                    else if(splitt[1].equals("alive"))
+                    {
+                        for(Player p : gamestate.players){
+                            if(p.name == splitt[2])
+                            p.alive = false;
+                        }
+                    }
+                    else if(splitt[1].equals("score"))
+                    {
+                        for(Player p : gamestate.players){
+                            if(p.name == splitt[2])
+                            p.score = Integer.parseInt(splitt[3]);
+                        }
+                    }
+                    else if(splitt[1].equals("winner"))
+                    {
+                        gamestate.winner = splitt[2];
+                    }
+                                        
+                    
+                    
+				}
+				if(splitt[0].equals("Ack"))
+				{
+					String one = splitt[1];
+					String ips = splitt[3];
+					String[] ipss = ips.split("^");
+					for(int i=0;i<ipss.length;i++)
+					{
+						if(ipss[i].length()!=0)
 						{
-							String ip = splitted[1];
-							String data = splitted[2];
-							Sender s = new Sender(this.Port,ip);
+							h.listOfIps.add(ipss[i]);
+							Sender s = new Sender(ipss[i]);
 							h.listOfSenders.add(s);
-							
-							//h.handleFWDData(data);
 						}
-
 					}
-
-
+					
+						h.getGameStateOnJoin(splitted[1],splitted[3],splitted[5]);
+					
+				}
+				if(h.isServer)
+				{
+					String[] splitter = str.split("~");
+					if(splitter[0].equals("Hello"))
+					{	
+						String ips ="";
+						int i=h.listOfSenders.size()+1;
+						for(Sender send: h.listOfSenders)
+						{
+							send.normalSend("FWD~"+IP+"~"+splitter[1]+"~"+i);
+							
+						}
+						
+						for(String ipss:h.listOfIps)
+						{
+							ips = ips+ipss+"^";
+						}
+						
+						double xc,yc;
+						String orient;
+						if(splitter[1] == "TOP"){
+						    orient = "HORIZONTAL";
+						    xc = 350;
+						    yc = 16;
+						}
+						else if(splitter[1] == "BOTTOM"){
+						    orient = "HORIZONTAL";
+						    xc = 350;
+						    yc = 684;
+						}
+						else if(splitter[1] == "LEFT"){
+						    orient = "VERTICAL";
+						    xc = 16;
+						    yc = 350;
+						}
+						else if(splitter[1] == "RIGHT"){
+						    orient = "VERTICAL";
+						    xc = 684;
+						    yc = 350;
+						}
+					    Player pl = new Player(i+"",IP,splitter[1], new Paddle(150,orient,xc,yc),gamestate.getWall(side));
+					    h.gamestate.addPlayer(pl);
+					
+					Sender s = new Sender(IP);
+					h.listOfSenders.add(s);
+					h.listOfIps.add(IP);
+					//list ofIps also has to be added
+					
+					s.normalSend("Ack~" + h.sendGameStateOnJoin(splitter[0])+"~IPS~"+ips+"~PlayerNo~"+i);
+					}
+				}
+				else if(!h.isServer)
+				{
+					String[] splitted = str.split("~");
+					if(splitted[0].equals("FWD"))
+					{
+						String ip = splitted[1];
+						String data = splitted[2];
+						Sender s = new Sender(ip);
+						h.listOfSenders.add(s);
+						h.listOfIps.add(ip);
+						h.handleFWDData(data);
+					}
+					
+				}
 					
 				
-			}
 			}
 			catch(IOException e){
 				System.out.println(e.toString()+"\nUnnable to receive");
